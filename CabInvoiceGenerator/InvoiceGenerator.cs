67530a792d290fc3eb0e62195251ccd41e3d633e
your Name <mohamedabdulayubm@gmail.com>
2022-07-19 20:09:20 +0530

    [Ayub] Calculated Fare

--- CabInvoiceGenerator/InvoiceGenerator.cs
+++ CabInvoiceGenerator/InvoiceGenerator.cs
@@ -11,12 +11,12 @@ namespace CabInvoiceGenerator
 
         const double RATE_PER_KM = 10,
                      RATE_PER_MIN = 1,
-                     MIN_FARE=5;
+                     MIN_FARE = 5;
         public double Fare(double km, double min)
         {
             double CalculatedFare = km * RATE_PER_KM + min * RATE_PER_MIN;
 
-            if(CalculatedFare >= MIN_FARE)
+            if (CalculatedFare >= MIN_FARE)
             {
                 return CalculatedFare;
             }
